7aef415ecf70d33affe19eee7ec212b9cdf29ecb
Shayla White <shayladwhite1@gmail.com>
2019-05-26 23:14:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
+def sort_array_asc(integer)
+  integer.each_with_index do |integer, index|
+     index += 1 
+      puts "#{index}. #{integer}"
+    end
+      integer.reverse!
+end
 
+def sort_array_desc(integer)
+  integer.each_with_index do |integer, index|
+     index -= 1 
+      puts "#{index}. #{integer}"
+    end
+end
