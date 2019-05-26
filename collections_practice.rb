0069827f5f519a855847e502c3ac78389c766c1a
Shayla White <shayladwhite1@gmail.com>
2019-05-26 23:32:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,8 +7,7 @@ def sort_array_asc(integer)
 end
 
 def sort_array_desc(integer)
-  integer.sort.each_with_index do |integer, index|
-     index += 1 
-      puts "#{index}. #{integer}"
-    end
+   do |b,a|
+    a <=> b
+  end
 end
