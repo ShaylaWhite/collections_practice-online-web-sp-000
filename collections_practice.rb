bc8277d0d821e1131becb216867a5b1ff7a30c92
Shayla White <shayladwhite1@gmail.com>
2019-05-26 23:44:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,9 +18,6 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array do |array|
-   array << array [2], array[3]
-   
-   array
-  end
+  array[1], array[2] = array[2], array[1]
+  array
 end
